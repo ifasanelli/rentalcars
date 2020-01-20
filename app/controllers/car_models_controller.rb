7bfbb977b45881e17b45eb3ad446d64feaa998ef
ifasanelli <fasanelli.italo@gmail.com>
2020-01-19 22:07:37 -0300

    [task] CarModel: register car model

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -6,4 +6,24 @@ class CarModelsController < ApplicationController
     def show
         @car_model = CarModel.find(params[:id])
     end
+
+    def new
+        @car_categories = CarCategory.all 
+        @manufacturers = Manufacturer.all
+        @car_model = CarModel.new
+    end
+
+    def create
+        @car_categories = CarCategory.all 
+        @manufacturers = Manufacturer.all
+        @car_model = CarModel.new(car_model_params)
+        @car_model.save
+        redirect_to @car_model
+    end
+
+    private
+
+    def car_model_params
+        params.require(:car_model).permit(:name, :motorization, :year, :fuel_type, :car_category_id, :manufacturer_id)
+    end
 end
---
+++ spec/features/admin_register_car_model_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'Admin register car model' do
+    scenario 'successfully' do
+        CarCategory.create!(name: 'A',
+            daily_rate: 100.95,
+            car_insurance: 50.95,
+            third_party_insurance: 150.45)
+        Manufacturer.create!(name: 'Fiat')
+        User.create!(email: 'italo@italo.com', password:123456)
+        visit root_path
+        click_on 'Entrar'
+        within 'form' do
+            fill_in 'Email', with: 'italo@italo.com'
+            fill_in 'Senha', with: '123456'
+            click_on 'Entrar'
+        end
+
+        click_on 'Modelos'
+        click_on 'Novo modelo'
+        fill_in 'Nome', with: 'Sienna'
+        fill_in 'Motorização', with: '1.6'
+        fill_in 'Ano', with: '2010'
+        fill_in 'Tipo combustível', with: 'Gasolina'
+        select 'A', from: 'Categoria'
+        select 'Fiat', from: 'Fabricante'
+        click_on 'Salvar'
+
+        expect(page).to have_content('Sienna')
+        expect(page).to have_content('2010')
+        expect(page).to have_content('1.6')
+        expect(page).to have_content('Gasolina')
+        expect(page).to have_content(/A/)
+        expect(page).to have_content('Fiat')
+    end
+end
